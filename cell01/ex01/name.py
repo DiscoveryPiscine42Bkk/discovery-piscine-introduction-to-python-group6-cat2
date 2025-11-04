67721da213c4d5e74e289ee32ca68e33deb9a195
Isaree’s Mm <68160058@kmitl.ac.th>
2025-11-04 10:07:57 +0000

    msg

--- cell01/ex01/name.py
+++ cell01/ex01/name.py
@@ -1,7 +1,7 @@
-last_name = "Isaree"
+first_name = "Isaree"
 
-first_name = "Sompornpattana"
+last_name = "Sompornpattana"
 
-whole_name = last_name + " " + first_name
+whole_name = first_name + " " + last_name
 
 print(whole_name)
